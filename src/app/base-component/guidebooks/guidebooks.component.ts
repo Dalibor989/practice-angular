061e6fba6c3ea468c584b96b317b882e95dc2e27
Dalibor989 <lazic_dalibor@hotmail.com>
2021-12-02 12:02:56 +0100

    restructuring, logut functionality, error handling

---
+++ src/app/base-component/guidebooks/guidebooks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guidebooks',
+  templateUrl: './guidebooks.component.html',
+  styleUrls: ['./guidebooks.component.css']
+})
+export class GuidebooksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
